cf9586a62ecd87b88782ed3bbfb9756b636e5c42
Juan C. Orozco <juanco89@gmail.com>
2021-10-08 18:12:06 -0500

    feat: se usa state en componentes para actualizar lista de mensajes

--- src/components/Contenedor.tsx
+++ src/components/Contenedor.tsx
@@ -3,19 +3,37 @@ import "./Contenedor.css"
 import ChatHistory from "./ChatHistory";
 import Mensaje from "./Mensaje";
 
-export default class Contenedor extends React.Component {
+type HistoryState = {
+    historial: string[]
+};
 
-    enviarMensaje(evento: string) {
-        console.log('ando en el contenedor', evento)
+export default class Contenedor extends React.Component<{}, HistoryState> {
+
+    mensajes: string[] = [];
+
+    constructor(props: any) {
+        super(props);
+        
+        this.state = { historial: [] };
+        
+        this.actualizarEstado = this.actualizarEstado.bind(this);
+    }
+    
+    enviarMensaje(mensaje: string) {
+        this.mensajes.push(mensaje);
+        this.actualizarEstado();
+    }
+
+    actualizarEstado() {
+        this.setState( { historial: this.mensajes } );
     }
 
     render() {
         return (
             <div id="contenedor">
-            <ChatHistory/>
-            <Mensaje onMensajeEnviado={this.enviarMensaje}/>
+            <ChatHistory historial={this.state.historial} />
+            <Mensaje onMensajeEnviado={(e: string) => this.enviarMensaje(e) }/>
             </div>
         );
     }
-
 }
